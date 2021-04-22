695004465e5f70c9d028e6d17685a3bff4700b6c
moolightwatch <moolightwatch@hotmail.com>
2021-04-22 20:20:49 +0800

    update

--- build_linux.sh
+++ build_linux.sh
@@ -1,3 +1,2 @@
 
-CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -a -ldflags " -s -w -linkmode 'external' -extldflags '-static'" -o porncrawler.exe ./main
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -a -ldflags " -s -w -linkmode 'external' -extldflags '-static'" -o porncrawler ./main
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -a -ldflags " -v -s -w -linkmode 'external' -extldflags '-static'" -o porncrawler ./main
